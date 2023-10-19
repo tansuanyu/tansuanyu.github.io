c14f8eb5fb5b72b2280f7fb3bcb40daab751a67d
DHC <212066122@qq.com>
2023-10-19 18:35:12 +0800

    Site updated: 2023-10-19 18:35:10

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,1 @@
-var posts=["2023/10/15/建站/基于Github的Hexo博客/","2020/06/29/网络安全/PHP跨站脚本漏洞解析/","2020/07/31/网络安全/PHP反序列化漏洞解析/","2020/06/09/网络安全/开放式重定向漏洞解析/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2023/10/15/建站/基于Github的Hexo博客/","2020/07/31/网络安全/PHP反序列化漏洞解析/","2020/06/09/网络安全/开放式重定向漏洞解析/","2020/06/29/网络安全/PHP跨站脚本漏洞解析/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
